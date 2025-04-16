587057853a00770ff07d8aaba2ea27d4f2e502ed
AnikaTabassum217 <tabassum.anika579@gmail.com>
2025-04-16 21:23:25 +0600

    Create Product form and collect product data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import ControlledField from './components/ControlledField/ControlledField'
 import HookForm from './components/HookForm/HookForm'
+import ProductManagement from './components/ProductManagement/ProductManagement'
 import FormAction from './components/SimpleForm/FormAction/FormAction'
 import SimpleForm from './components/SimpleForm/SimpleForm'
 import UncontrolledField from './components/UncontrolledField/UncontrolledField'
@@ -13,7 +14,8 @@ function App() {
       {/* <FormAction></FormAction> */}
       {/* <ControlledField></ControlledField> */}
       {/* <UncontrolledField></UncontrolledField> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      <ProductManagement></ProductManagement>
     </>
   )
 }
